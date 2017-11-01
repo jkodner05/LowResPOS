ef55a85731217c65f6f52d5999212ac368e13ab5
Jordan A Kodner <jkodner@seas.upenn.edu>
2017-11-01 12:55:43 -0400

    create wordlist files as input to morpho segmentation

---
+++ miscfiles/get_wordlists_allutb.sh
@@ -0,0 +1,10 @@
+cd ..
+python get_wordlists.py /mnt/nlpgridio2/nlp/users/lorelei/uni-dep-tb/universal_treebanks_v2.0/std/fr /mnt/nlpgridio2/nlp/users/jkodner/lowrespos_data/wordlists/utb_fr.wordlist -c utb
+python get_wordlists.py /mnt/nlpgridio2/nlp/users/lorelei/uni-dep-tb/universal_treebanks_v2.0/std/de /mnt/nlpgridio2/nlp/users/jkodner/lowrespos_data/wordlists/utb_de.wordlist -c utb
+python get_wordlists.py /mnt/nlpgridio2/nlp/users/lorelei/uni-dep-tb/universal_treebanks_v2.0/std/id /mnt/nlpgridio2/nlp/users/jkodner/lowrespos_data/wordlists/utb_id.wordlist -c utb
+python get_wordlists.py /mnt/nlpgridio2/nlp/users/lorelei/uni-dep-tb/universal_treebanks_v2.0/std/it /mnt/nlpgridio2/nlp/users/jkodner/lowrespos_data/wordlists/utb_it.wordlist -c utb
+python get_wordlists.py /mnt/nlpgridio2/nlp/users/lorelei/uni-dep-tb/universal_treebanks_v2.0/std/ja /mnt/nlpgridio2/nlp/users/jkodner/lowrespos_data/wordlists/utb_ja.wordlist -c utb
+python get_wordlists.py /mnt/nlpgridio2/nlp/users/lorelei/uni-dep-tb/universal_treebanks_v2.0/std/ko /mnt/nlpgridio2/nlp/users/jkodner/lowrespos_data/wordlists/utb_ko.wordlist -c utb
+python get_wordlists.py /mnt/nlpgridio2/nlp/users/lorelei/uni-dep-tb/universal_treebanks_v2.0/std/pt-br /mnt/nlpgridio2/nlp/users/jkodner/lowrespos_data/wordlists/utb_pt-br.wordlist -c utb
+python get_wordlists.py /mnt/nlpgridio2/nlp/users/lorelei/uni-dep-tb/universal_treebanks_v2.0/std/es /mnt/nlpgridio2/nlp/users/jkodner/lowrespos_data/wordlists/utb_es.wordlist -c utb
+python get_wordlists.py /mnt/nlpgridio2/nlp/users/lorelei/uni-dep-tb/universal_treebanks_v2.0/std/sv /mnt/nlpgridio2/nlp/users/jkodner/lowrespos_data/wordlists/utb_sv.wordlist -c utb
